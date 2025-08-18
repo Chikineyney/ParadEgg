5f68d29c011ca98f81d2eea366a4d2bd9533cb3a
Chikineyney <agadharold3@gmail.com>
2025-08-18 23:21:15 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Chikineyney/ParadEgg/main/ParadEgg.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Chikineyney/ParadEgg/refs/heads/main/ParadEgg.lua"))()
